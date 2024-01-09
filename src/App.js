a179f98fb0bef1f0e5589e3f088c486ab1db5a7f
SHIVRAJ KUMAR <shivrajkumar11math@gmail.com>
2024-01-09 19:19:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Feed />}></Route>
+        <Route exact path="/video/:id" element={<VideoDetail />}></Route>
+        <Route exact path="/channel/:id" element={<ChannelDetail />}></Route>
+        <Route
+          exact
+          path="/search/:searchTerm"
+          element={<SearchFeed />}
+        ></Route>
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  // console.log('fetchFromAPI called...');
+  try {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    // console.log(data);
+    return data;
+  } catch (error) {
+    console.log("Error occured during the fetch from API");
+    console.log(error);
+    return [];
+  }
+};
